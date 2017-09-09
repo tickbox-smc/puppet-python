8ee281c322f10af327af1efff591f2e100dccb6d
root <root@puppet.tickbox>
2017-09-09 00:40:58 +0000

    bug fix init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,10 @@
 # ===========================
 #
 
-class python {
+class python (
   String $ensure,
   String $package_name,
-
+){
   # default values are in python/functions/data.pp
   class { '::python::install': }
 }
