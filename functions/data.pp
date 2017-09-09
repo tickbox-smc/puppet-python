57b568824f30f0412da4558b2b0918ef341f3f01
root <root@puppet.tickbox>
2017-09-09 00:21:13 +0000

    bug fix

--- functions/data.pp
+++ functions/data.pp
@@ -2,8 +2,8 @@
 
 function python::data() {
   $base_params = {
-    'python::ensure'      => installed,
-    'python::package_name => 'python36',
+    'python::ensure'       => installed,
+    'python::package_name' => 'python36',
   }
   # Merge the hashes and return a single hash.
   $base_params
