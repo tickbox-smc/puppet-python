f087bb93b55e8d17a1de7ca96d491773822ab1a5
root <root@puppet.tickbox>
2017-09-13 14:16:08 +0000

    add new params

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,11 @@
 class python (
   String $ensure,
   String $package_name,
+  String $link_ensure,
+  String $phyton_name,
+  String $local_python_name,
+  String $local_bin,
+  String $install_bin,
 ){
   # default values are in python/functions/data.pp
   class { '::python::install': }
